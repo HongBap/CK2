49cc091b79d55900503be48bf30be06e3950d897
Hồng Ngô <7679429-hongbap@users.noreply.gitlab.com>
2022-04-17 22:47:51 +0700

    Add project files.

---
+++ SignalR/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.Models
+{
+    public class Employee
+    {
+        public int Id { set; get; }
+        public string MaCK { set; get; }
+        public string GiaMua1 { set; get; }
+        public string SoLuongMua1 { set; get; }
+        public string GiaMua2 { set; get; }
+        public string SoLuongMua2 { set; get; }
+        public string GiaMua3 { set; get; }
+        public string SoLuongMua3 { set; get; }
+        public string GiaKhop { set; get; }
+        public string SoLuongKhop { set; get; }
+        public string GiaBan1 { set; get; }
+        public string SoLuongBan1 { set; get; }
+        public string GiaBan2 { set; get; }
+        public string SoLuongBan2 { set; get; }
+        public string GiaBan3 { set; get; }
+        public string SoLuongBan3 { set; get; }
+
+        public string TongSo { set; get; }
+    }
+}
---
+++ SignalR/Models/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.Models
+{
+    public class Repository
+    {
+        SqlConnection con = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        public List<Employee> GetAllMessages()
+        {
+            var messages = new List<Employee>();
+            using (var cmd = new SqlCommand(@"SELECT [MaCK],  
+                                           [GiaMua1],[SoLuongMua1],[GiaMua2],[SoLuongMua2],[GiaMua3],[SoLuongMua3],
+                                            [GiaKhop],[SoLuongKhop],[GiaBan1],[SoLuongBan1],[GiaBan2],[SoLuongBan2],
+                                            [GiaBan3],[SoLuongBan3],[TongSo] FROM [dbo].[BangGiaTrucTuyen]", con))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                var dependency = new SqlDependency(cmd);
+                dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    messages.Add(item: new Employee
+                    {
+              
+                        MaCK = ds.Tables[0].Rows[i][0].ToString(),
+                        GiaMua1 = ds.Tables[0].Rows[i][1].ToString(),
+                        SoLuongMua1 = ds.Tables[0].Rows[i][2].ToString(),
+                        GiaMua2 = ds.Tables[0].Rows[i][3].ToString(),
+                        SoLuongMua2 = ds.Tables[0].Rows[i][4].ToString(),
+                        GiaMua3 = ds.Tables[0].Rows[i][5].ToString(),
+                        SoLuongMua3 =ds.Tables[0].Rows[i][6].ToString(),
+                        GiaKhop = ds.Tables[0].Rows[i][7].ToString(),
+                        SoLuongKhop = ds.Tables[0].Rows[i][8].ToString(),
+                        GiaBan1 = ds.Tables[0].Rows[i][9].ToString(),
+                        SoLuongBan1 = ds.Tables[0].Rows[i][10].ToString(),
+                        GiaBan2 = ds.Tables[0].Rows[i][11].ToString(),
+                        SoLuongBan2 = ds.Tables[0].Rows[i][12].ToString(),
+                        GiaBan3 = ds.Tables[0].Rows[i][13].ToString(),
+                        SoLuongBan3 = ds.Tables[0].Rows[i][14].ToString(),
+                        TongSo = ds.Tables[0].Rows[i][15].ToString(),
+                    });
+                }
+            }
+            return messages;
+        }
+
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e) //this will be called when any changes occur in db table. 
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                MyHub.SendMessages();
+            }
+        }
+    }
+}
